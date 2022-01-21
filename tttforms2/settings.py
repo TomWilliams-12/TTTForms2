6f4fbebeeff4468284640259d0598aec0b9c9476
williams.t17@gmail.com <williams.t17@gmail.com>
2022-01-21 20:36:50 +0000

    updated database settings

--- tttforms2/settings.py
+++ tttforms2/settings.py
@@ -99,29 +99,18 @@ WSGI_APPLICATION = 'tttforms2.wsgi.application'
 #     }
 # }
 
+
 if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
-        '    ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.mysql',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
             'HOST': os.environ['RDS_HOSTNAME'],
             'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
-else:
-    env_vars = get_environ_vars()
-    DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': env_vars['RDS_DB_NAME'],
-        'USER': env_vars['RDS_USERNAME'],
-        'PASSWORD': env_vars['RDS_PASSWORD'],
-        'HOST': env_vars['RDS_HOSTNAME'],
-        'PORT': env_vars['RDS_PORT'],
-    }
-}
 
 
 # Password validation
