548a7f78662f3c7ee162690b0e0569dd2939188a
williams.t17@gmail.com <williams.t17@gmail.com>
2022-01-21 13:03:29 +0000

    updated settings for database access in elb

--- tttforms2/settings.py
+++ tttforms2/settings.py
@@ -1,5 +1,19 @@
-import os
 from pathlib import Path
+import os
+import subprocess
+import ast
+
+
+def get_environ_vars():
+    completed_process = subprocess.run(
+        ['/opt/elasticbeanstalk/bin/get-config', 'environment'],
+        stdout=subprocess.PIPE,
+        text=True,
+        check=True
+    )
+
+    return ast.literal_eval(completed_process.stdout)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,14 +99,27 @@ WSGI_APPLICATION = 'tttforms2.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+        '    ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+    }
+}
+else:
+    env_vars = get_environ_vars()
+    DATABASES = {
+        'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ['RDS_DB_NAME'],
-        'USER': os.environ['RDS_USERNAME'],
-        'PASSWORD': os.environ['RDS_PASSWORD'],
-        'HOST': os.environ['RDS_HOSTNAME'],
-        'PORT': os.environ['RDS_PORT'],
+        'NAME': env_vars['RDS_DB_NAME'],
+        'USER': env_vars['RDS_USERNAME'],
+        'PASSWORD': env_vars['RDS_PASSWORD'],
+        'HOST': env_vars['RDS_HOSTNAME'],
+        'PORT': env_vars['RDS_PORT'],
     }
 }
 
