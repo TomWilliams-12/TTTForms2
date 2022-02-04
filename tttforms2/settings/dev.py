4c60a8fab6f070fb859aca98be187a567fcecde8
williams.t17@gmail.com <williams.t17@gmail.com>
2022-02-04 19:37:12 +0000

    test

--- tttforms2/settings/dev.py
+++ tttforms2/settings/dev.py
@@ -22,13 +22,14 @@ DEBUG = int(os.environ.get('DEBUG', default=1))
 
 ALLOWED_HOSTS = ['tttforms-dev.eu-west-2.elasticbeanstalk.com']
 
-DATABASES = {
-    'default': {
-    '    ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ['RDS_DB_NAME'],
-        'USER': os.environ['RDS_USERNAME'],
-        'PASSWORD': os.environ['RDS_PASSWORD'],
-        'HOST': os.environ['RDS_HOSTNAME'],
-        'PORT': os.environ['RDS_PORT'],
-    }
-}
+if 'RDS_HOSTNAME' in os.environ:
+     DATABASES = {
+         'default': {
+         '    ENGINE': 'django.db.backends.mysql',
+              'NAME': os.environ['RDS_DB_NAME'],
+              'USER': os.environ['RDS_USERNAME'],
+              'PASSWORD': os.environ['RDS_PASSWORD'],
+              'HOST': os.environ['RDS_HOSTNAME'],
+              'PORT': os.environ['RDS_PORT'],
+     }
+ }
